cba8ba33e561e2700f82d5efbb305c076db06689
Aubrey Polk <aubstacle3@gmail.com>
2020-10-30 12:11:39 -0400

    built html-route and got main page displayed

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
   db.Workout.find().then((workout) => {
-    res.json.(workout);
+    res.json(workout);
   });
 });
 
